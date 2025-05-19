f37f0a17d03d1d29a5c17080446cd8ddbc0730f3
Victor Moraes <victormoraespt4@gmail.com>
2025-05-19 01:19:09 -0300

    new response helpers

--- internal/adapters/endpoints/posthandler.go
+++ internal/adapters/endpoints/posthandler.go
@@ -25,6 +25,7 @@ func NewPostHandler(api chi.Router, serv core.PostService) {
 	api.Route("/post", func(r chi.Router) {
 		r.Get("/", handler.GetAll)
 		r.Get("/{id}", handler.Get)
+		r.Get("/{tagId}", handler.GetByTag)
 		r.Post("/", handler.Get)
 		r.Put("/", handler.Update)
 	})
@@ -33,85 +34,84 @@ func NewPostHandler(api chi.Router, serv core.PostService) {
 func (ph *PostHandler) GetAll(w http.ResponseWriter, r *http.Request) {
 	posts, err := ph.serv.GetAll(r.Context())
 	if err != nil {
-		response.Send(w, http.StatusInternalServerError, err)
+		response.SendInternalServerError(w, err)
 		return
 	}
 
-	response.Send(w, http.StatusOK, posts)
+	response.SendOk(w, posts)
 }
 
 func (ph *PostHandler) Get(w http.ResponseWriter, r *http.Request) {
 	idPath := r.PathValue("id")
 	if idPath == "" {
-		response.Send(w, http.StatusBadRequest, ErrIdIsRequired)
+		response.SendBadRequest(w, ErrIdIsRequired)
 		return
 	}
 
 	id, err := strconv.ParseUint(idPath, 10, 64)
 	if err != nil {
-		response.Send(w, http.StatusInternalServerError, err)
+		response.SendInternalServerError(w, err)
 		return
 	}
 
 	post, err := ph.serv.Get(r.Context(), uint(id))
 	if err != nil {
-		response.Send(w, http.StatusInternalServerError, err)
+		response.SendInternalServerError(w, err)
 		return
 	}
 
-	response.Send(w, http.StatusOK, post)
+	response.SendOk(w, post)
 }
 
 func (ph *PostHandler) GetByTag(w http.ResponseWriter, r *http.Request) {
 	idPath := r.PathValue("id")
 	if idPath == "" {
-		response.Send(w, http.StatusInternalServerError, ErrTagIdIsRequired)
+		response.SendBadRequest(w, ErrTagIdIsRequired)
 		return
 	}
 
 	tagID, err := strconv.ParseUint(idPath, 10, 64)
 	if err != nil {
-		response.Send(w, http.StatusInternalServerError, err)
+		response.SendInternalServerError(w, err)
 		return
 	}
 
 	p, err := ph.serv.GetByTag(r.Context(), uint(tagID))
 	if err != nil {
-		response.Send(w, http.StatusInternalServerError, err)
+		response.SendInternalServerError(w, err)
 		return
 	}
 
-	response.Send(w, http.StatusOK, p)
+	response.SendOk(w, p)
 }
 
 func (ph *PostHandler) Create(w http.ResponseWriter, r *http.Request) {
 	var i core.PostInput
 	if err := json.NewDecoder(r.Body).Decode(&i); err != nil {
-		response.Send(w, http.StatusBadRequest, err)
+		response.SendBadRequest(w, err)
 		return
 	}
 
 	post, err := ph.serv.Create(r.Context(), i)
 	if err != nil {
-		response.Send(w, http.StatusInternalServerError, err)
+		response.SendInternalServerError(w, err)
 		return
 	}
 
-	response.Send(w, http.StatusCreated, post)
+	response.SendCreated(w, post)
 }
 
 func (ph *PostHandler) Update(w http.ResponseWriter, r *http.Request) {
 	var i core.PostInput
 	if err := json.NewDecoder(r.Body).Decode(&i); err != nil {
-		response.Send(w, http.StatusBadRequest, err)
+		response.SendBadRequest(w, err)
 		return
 	}
 
 	post, err := ph.serv.Update(r.Context(), i)
 	if err != nil {
-		response.Send(w, http.StatusInternalServerError, err)
+		response.SendInternalServerError(w, err)
 		return
 	}
-
-	response.Send(w, http.StatusOK, post)
+	response.SendOk(w, post)
 }
